19a36f6da13271818d7cb0ebd706cba2e3cd25fc
denersonpinas <denersonpinas19@gmail.com>
2023-09-01 17:35:38 -0300

    :perf: Finalizing the Performance Initial

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import ButtonsPrimary from '@/src/components/buttons/primary'
+import './dashboard.scss'
 
 export default function Dashboard({text}) {
   return (
     <div>
-      <h1>{text}</h1>
+      <h1 className="title">{text}</h1>
       <ButtonsPrimary>
         Click Here!
       </ButtonsPrimary>
